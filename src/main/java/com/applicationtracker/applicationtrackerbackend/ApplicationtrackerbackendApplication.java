29b6f99f091bd32c6637e46640cde278a8cc58bb
Parth Chheda <parth.h.chheda@gmail.com>
2025-08-03 16:27:16 -0700

    init commit

---
+++ src/main/java/com/applicationtracker/applicationtrackerbackend/ApplicationtrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.applicationtracker.applicationtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationtrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationtrackerbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applicationtracker/applicationtrackerbackend/ApplicationtrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applicationtracker.applicationtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationtrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
